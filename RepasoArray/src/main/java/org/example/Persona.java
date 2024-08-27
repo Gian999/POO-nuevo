7f56bcc96ebbcb822af1ef3b222a14e0720fdd4f
Gianluck <giancetpro@gmail.com>
2024-08-27 10:32:31 -0500

    update

--- RepasoArray/src/main/java/org/example/Persona.java
+++ RepasoArray/src/main/java/org/example/Persona.java
@@ -1,4 +1,4 @@
-package pe.edu.upeu.sysgui.modelo;
+package org.example;
 
 import java.util.Date;
 
@@ -6,29 +6,38 @@ public class Persona {
     protected String nombre;
     protected String estadoAsis;
     char genero;
-    public Date fechaReg;
+    Date fechaReg;
+
     public Persona(){}
+
     public Persona(String nombre, String estadoAsis, Date fechaReg){
         this.nombre = nombre;
         this.estadoAsis = estadoAsis;
         this.fechaReg = fechaReg;
     }
+
     public Persona(String nombre, String estadoAsis, char genero) {
         this.nombre = nombre;
         this.estadoAsis = estadoAsis;
         this.genero=genero;
     }
+
     public String getNombre() {
-        return nombre;}
+        return nombre;
+    }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     public String getEstadoAsis() {
         return estadoAsis;
     }
+
     public void setEstadoAsis(String estadoAsis) {
         this.estadoAsis = estadoAsis;
     }
+
     @Override
     public String toString() {
         return this.nombre +"\t"+ this.estadoAsis +"\t"+ this.fechaReg;
